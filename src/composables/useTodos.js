7944119d7269bc76342e5d7a608e3e6f40df1561
sky <yepwaibin@gmail.com>
2021-07-30 15:42:22 +0800

    TodoList 小demo

---
+++ src/composables/useTodos.js
@@ -0,0 +1,11 @@
+import { ref } from "vue";
+
+export default function useTodos() {
+  const todos = ref([]);
+  // 添加 todo
+  const addTodo = todo => todos.value.push(todo);
+  return {
+    todos,
+    addTodo
+  };
+}
